e421544736a55b498a480f501a3f0606698b9f82
juliannelicon@gmail.com <juliannelicon@gmail.com>
2021-12-20 09:50:07 -0300

    Altera configurações do grid na página inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Grid, Paper } from '@mui/material';
 
+import Aniversariantes from '../components/Aniversariantes';
 import GridServicos from '../components/GridServicos';
 import GridSistemas from '../components/GridSistemas';
 import Noticias from '../components/Noticias';
@@ -7,7 +8,58 @@ import Noticias from '../components/Noticias';
 export default function Home() {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} sm={12} md={6} lg={4}>
+      <Grid item xs={12} sm={12} md={6} lg={7} xl={8}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              <GridSistemas />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              <GridServicos />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} sm={12} md={6} lg={5} xl={4}>
+        <Paper
+          sx={{
+            p: 2,
+            display: 'flex',
+            flexDirection: {
+              xs: 'column',
+              sm: 'column',
+              md: 'column',
+              lg: 'column',
+            },
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Noticias />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} sm={12} md={6} lg={7} xl={8}>
         <Paper
           sx={{
             p: 2,
@@ -17,25 +69,11 @@ export default function Home() {
             height: '100%',
           }}
         >
-          <GridSistemas />
+          {/* <Agenda /> */}
         </Paper>
       </Grid>
 
-      <Grid item xs={12} sm={12} md={6} lg={4}>
-        <Paper
-          sx={{
-            p: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <GridServicos />
-        </Paper>
-      </Grid>
-
-      <Grid item xs={12} sm={12} md={12} lg={4}>
+      <Grid item xs={12} sm={12} md={6} lg={5} xl={4}>
         <Paper
           sx={{
             p: 2,
@@ -50,21 +88,7 @@ export default function Home() {
             height: '100%',
           }}
         >
-          <Noticias />
-        </Paper>
-      </Grid>
-
-      <Grid item xs={12} sm={12} md={12} lg={8}>
-        <Paper
-          sx={{
-            p: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          Agenda
+          <Aniversariantes />
         </Paper>
       </Grid>
     </Grid>
