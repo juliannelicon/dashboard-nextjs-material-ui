fc946f9fe9355a46883cd43f28f47a8fa94ee403
juliannelicon@gmail.com <juliannelicon@gmail.com>
2021-12-20 09:47:42 -0300

    Altera comportamento de notícias

--- src/components/Noticias/index.tsx
+++ src/components/Noticias/index.tsx
@@ -16,7 +16,7 @@ const myLoader = ({ src, width }: { src: string; width: number }) => {
 export default function Noticias() {
   return (
     <>
-      <Box display="flex" flexDirection="column" sx={{ marginLeft: { lg: 2 } }}>
+      <Box display="flex" flexDirection="column">
         <Image
           loader={myLoader}
           src="/image.jpg"
@@ -26,15 +26,16 @@ export default function Noticias() {
           height={200}
         />
         <Typography fontWeight="bold" marginTop={1} marginBottom={2}>
-          Digite a descrição da notícia com no máximo 1 linha
+          Título da notícia com no máximo 2 linha
         </Typography>
       </Box>
-      <List sx={{ marginLeft: { md: 2 } }}>
+
+      <List>
         <Divider variant="inset" component="li" sx={{ ml: 0 }} />
         {[1, 2, 3].map(noticia => (
           <ListItem key={noticia} disablePadding>
             <ListItem button disableGutters divider>
-              <ListItemText primary="Digite a descrição da notícia com no máximo 1 linha" />
+              <ListItemText primary="Título da notícia com no máximo 2 linha" />
             </ListItem>
           </ListItem>
         ))}
