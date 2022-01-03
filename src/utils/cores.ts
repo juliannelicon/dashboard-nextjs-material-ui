7fbd943ba93dd800e7983ab0a214c7ffe386ca1c
juliannelicon@gmail.com <juliannelicon@gmail.com>
2022-01-03 15:27:44 -0300

    Adiciona arquivos utils cores

---
+++ src/utils/cores.ts
@@ -0,0 +1,22 @@
+import { colors } from '@mui/material';
+
+export const cores = [
+  { id: 1, label: 'brown', color: colors.brown },
+  { id: 2, label: 'grey', color: colors.grey },
+  { id: 3, label: 'blueGrey', color: colors.blueGrey },
+  { id: 4, label: 'cyan', color: colors.cyan },
+  { id: 6, label: 'blue', color: colors.blue },
+  { id: 7, label: 'indigo', color: colors.indigo },
+  { id: 8, label: 'deepPurple', color: colors.deepPurple },
+  { id: 9, label: 'purple', color: colors.purple },
+  { id: 10, label: 'pink', color: colors.pink },
+  { id: 11, label: 'red', color: colors.red },
+  { id: 12, label: 'deepOrange', color: colors.deepOrange },
+  { id: 13, label: 'orange', color: colors.orange },
+  { id: 14, label: 'amber', color: colors.amber, bg: colors.amber[500] },
+  { id: 15, label: 'yellow', color: colors.yellow },
+  { id: 16, label: 'lime', color: colors.lime },
+  { id: 18, label: 'teal', color: colors.teal },
+  { id: 19, label: 'green', color: colors.green },
+  { id: 20, label: 'green2', color: { 500: '#2e7d32' } },
+];
