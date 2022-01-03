a9d535df2a7a4c9659b2fa0795c4d34bddc8b605
juliannelicon@gmail.com <juliannelicon@gmail.com>
2022-01-03 15:26:38 -0300

    Adicona configurações de cores no context theme

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -1,15 +1,30 @@
-import { createContext, useContext, useMemo, useState } from 'react';
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
 
-import { createTheme, PaletteMode, Theme } from '@mui/material';
+import {
+  createTheme,
+  PaletteMode,
+  Theme,
+  PaletteColorOptions,
+  colors,
+} from '@mui/material';
 
 interface ThemeProviderProps {
   children: JSX.Element;
 }
 
 interface ThemeContextData {
-  mode: PaletteMode;
   theme: Theme;
-  toggleMode: () => void;
+  mode: PaletteMode;
+  setMode: Dispatch<SetStateAction<PaletteMode>>;
+  setColorPrimary: Dispatch<SetStateAction<PaletteColorOptions>>;
+  setColorSecondary: Dispatch<SetStateAction<PaletteColorOptions>>;
 }
 
 const ThemeContext = createContext({} as ThemeContextData);
@@ -18,34 +33,29 @@ export function AppThemeProvider({
   children,
 }: ThemeProviderProps): JSX.Element {
   const [mode, setMode] = useState<PaletteMode>('light');
-
-  const toggleMode = () => {
-    setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
-  };
+  const [colorPrimary, setColorPrimary] = useState<PaletteColorOptions>(
+    colors.indigo,
+  );
+  const [colorSecondary, setColorSecondary] = useState<PaletteColorOptions>(
+    colors.red,
+  );
 
   const theme = useMemo(
     () =>
       createTheme({
         palette: {
           mode,
-          primary: {
-            light: '#757ce8',
-            main: '#3f50b5',
-            dark: '#f1f1f1',
-            contrastText: '#fff',
-          },
-          secondary: {
-            light: '#ff7961',
-            main: '#f44336',
-            dark: '#ba000d',
-            contrastText: '#000',
-          },
+          primary: colorPrimary,
+          secondary: colorSecondary,
         },
       }),
-    [mode],
+    [mode, colorPrimary, colorSecondary],
   );
 
-  const value = useMemo(() => ({ mode, theme, toggleMode }), [mode, theme]);
+  const value = useMemo(
+    () => ({ theme, mode, setMode, setColorPrimary, setColorSecondary }),
+    [theme, mode],
+  );
 
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
@@ -53,5 +63,27 @@ export function AppThemeProvider({
 }
 
 export function useAppTheme() {
-  return useContext(ThemeContext);
+  const { theme, mode, setMode, setColorPrimary, setColorSecondary } =
+    useContext(ThemeContext);
+
+  const toggleMode = () => {
+    setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  const addColorPrimary = (color: PaletteColorOptions) => {
+    setColorPrimary(color);
+    console.log('addColorPrimary');
+  };
+
+  const addColorSecondary = (color: PaletteColorOptions) => {
+    setColorSecondary(color);
+  };
+
+  return {
+    theme,
+    mode,
+    toggleMode,
+    addColorPrimary,
+    addColorSecondary,
+  };
 }
